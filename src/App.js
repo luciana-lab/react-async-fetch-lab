a3d7736c1348e870be4b92dfb63864209a213b45
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-01 14:37:29 -0400

    commit tree 51feb94ffc92b58abd08d665d6b4480606063b2d

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React from 'react';
+class App extends React.Component {
+    state = {
+        spacePeople: []
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(resp => resp.json())
+            .then(data => this.setState({ spacePeople: data.people }))
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.state.spacePeople.map(person => <li>{`${person.name}`}</li>)}
+            </ul>
+        )
+    }
+}
+
+export default App;
